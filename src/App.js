300828b3e477f8154dcd0e6ca3fe9cbe69f19018
ajnas ashkath <ajnasashkath@gmail.com>
2026-03-10 12:57:37 +0530

    finished

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Education from './components/Education';
-import Footer from './components/Footer';
+
 
 function App() {
   return (
